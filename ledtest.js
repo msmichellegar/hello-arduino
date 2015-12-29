96117c5d69706f8f8396f44301454b25a9347633
msmichellegar <msmichellegar@gmail.com>
2015-12-29 16:10:16 +1100

    practice with leds

--- ledtest.js
+++ ledtest.js
@@ -4,14 +4,18 @@ var board = new five.Board();
 board.on("ready", function() {
   console.log("Ready!");
 
-  var green = new five.Led(13);
-  var orange = new five.Led(12);
-  var red = new five.Led(11);
-  var blue = new five.Led(10);
+  var rgb = new five.Led.RGB([13, 12, 11]);
 
-  green.strobe(100);
-  orange.strobe(300);
-  red.strobe(500);
-  blue.strobe(200);
+  // var blue = new five.Led(13);
+  // var orange = new five.Led(12);
+  // var red = new five.Led(11);
+  // var green = new five.Led(10);
+
+  // blue.strobe(1000);
+  // orange.strobe(300);
+  // red.strobe(500);
+  // blue.strobe(200);
+
+  rgb.color("#ff00ff");
 
 });
