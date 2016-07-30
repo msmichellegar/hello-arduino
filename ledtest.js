609396f8bd9950f753c796097ea641cfd8050ee4
msmichellegar <msmichellegar@gmail.com>
2016-07-30 13:38:00 +1000

    splits matrix into own file

--- ledtest.js
+++ ledtest.js
@@ -2,20 +2,12 @@ var five = require("johnny-five");
 var board = new five.Board();
 
 board.on("ready", function() {
-  console.log("Ready!");
+    var red = new five.Led(13);
+    var yellow = new five.Led(11);
+    var green = new five.Led(9);
 
-  var rgb = new five.Led.RGB([13, 12, 11]);
-
-  // var blue = new five.Led(13);
-  // var orange = new five.Led(12);
-  // var red = new five.Led(11);
-  // var green = new five.Led(10);
-
-  // blue.strobe(1000);
-  // orange.strobe(300);
-  // red.strobe(500);
-  // blue.strobe(200);
-
-  rgb.color("#ff00ff");
+    red.strobe(100);
+    yellow.strobe(300);
+    green.strobe(200);
 
 });
---
+++ matrixtest.js
@@ -0,0 +1,64 @@
+var five = require("johnny-five");
+var board = new five.Board();
+
+board.on("ready", function() {
+
+    var heart = [
+        "01100110",
+        "10011001",
+        "10000001",
+        "10000001",
+        "01000010",
+        "00100100",
+        "00011000",
+        "00000000"
+    ];
+
+    var square = [
+        "11111111",
+        "10000001",
+        "10000001",
+        "10000001",
+        "10000001",
+        "10000001",
+        "10000001",
+        "11111111"
+    ];
+
+    var matrix = new five.Led.Matrix({
+        pins: {
+            data: 2,    // DIN
+            clock: 3,   // CLK
+            cs: 4       // CS
+        }
+    });
+
+    matrix.on();
+
+    var msg = "hello".split("");
+
+    // Display each letter for 1 second
+    function next() {
+        var c;
+
+        if (c = msg.shift()) {
+            matrix.draw(c);
+            setTimeout(next, 400);
+        }
+    }
+
+    next();
+    matrix.draw(heart);
+
+    this.repl.inject({
+        matrix: matrix,
+        // Type "heart()" in the REPL to
+        // display a heart!
+        heart: function() {
+            matrix.draw(heart);
+        },
+        square: function() {
+            matrix.draw(square);
+        }
+    });
+});
